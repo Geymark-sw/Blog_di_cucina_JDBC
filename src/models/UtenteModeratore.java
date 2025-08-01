17658fa8bb6bfc8fc8f00f9ed0240ab9e4796110
Geymark-sw <emmanuelpapio12@gmail.com>
2025-08-01 17:24:06 +0200

    fnl

--- src/models/UtenteModeratore.java
+++ src/models/UtenteModeratore.java
@@ -2,6 +2,8 @@ package models;
 
 import java.util.Scanner;
 
+import utils.FunzioniUtilsUtente;
+
 public class UtenteModeratore extends Utente {
 	
 	private Scanner input = new Scanner(System.in);
@@ -44,18 +46,75 @@ public class UtenteModeratore extends Utente {
 		} while (scelta < 1 || scelta > 2);
 
 		switch(scelta) {
-		case "1":
-			modificaProfiloUtente(u);
-			break;
-		case "2":
+		case 1:
+			return modificaProfiloUtente(u);
+			
+		case 2:
+			return modificaPostUtente(u);
 			break;
 		default:
-			break;
+			return false;
 		}
 	}
 
-	private void modificaProfiloUtente(Utente u) {
+	private void modificaPostUtente(Utente u) {
 		// TODO Auto-generated method stub
+		
+	}
+
+	private Boolean modificaProfiloUtente(Utente u) {
+		FunzioniUtilsUtente.stampaAnagraficaUtente(u);
+		Integer scelta = null;
+		do {
+			System.out.println("Che cosa vuoi modificare?\n1. Nome" 
+													+ "\n2. Cognome"
+													+ "\n3. Nickname"
+													+ "\n4. Email"
+													+ "\n5. Password"
+													+ "\n6. Status");
+			try {
+				scelta = Integer.parseInt(input.nextLine());
+				if(scelta < 1 || scelta > 6) {
+					System.out.println("Valore non valido, riprova.");
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Valore non valido, riprova.");
+			}
+		} while (scelta < 1 || scelta > 6);
+		
+		switch(scelta) {
+		case 1:
+			System.out.println("Inserisci il nuovo nome:");
+			String nome = input.nextLine();
+			u.setNome(nome);
+			System.out.println("Modifica effettuata con successo!");
+			break;
+		case 2:
+			System.out.println("Inserisci il nuovo cognome:");
+			String cognome = input.nextLine();
+			u.setCognome(cognome);
+			System.out.println("Modifica effettuata con successo!");
+			break;
+		case 3:
+			System.out.println("Inserisci in nuovo nickname:");
+			String nickname = input.nextLine();
+			u.setNickname(nickname);
+			System.out.println("Modifica effettuata con successo!");
+		case 4:
+			System.out.println("Inserisci la nuova email:");
+			String email = input.nextLine();
+			u.setEmail(email);
+			System.out.println("Modifica Effettuata con successo!");
+		case 5:
+			System.out.println("Inserisci la nuova password:");
+			String password = input.nextLine();
+			u.setPassword(password);
+			System.out.println("Modifica effettuata con successo!");
+			break;
+		default:
+			return false;
+		}
+		return true;
 		
 	}
 	
